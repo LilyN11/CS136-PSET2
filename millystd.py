5a0654859264cf35fcd301a0b0b5c1ce2f81255f
LilyN11 <lilynguyen1011@gmail.com>
2023-09-27 00:52:26 -0400

    tried standard

--- millystd.py
+++ millystd.py
@@ -13,11 +13,11 @@ from messages import Upload, Request
 from util import even_split
 from peer import Peer
 
-class Dummy(Peer):
+class MillyStd(Peer):
     def post_init(self):
         print(("post_init(): %s here!" % self.id))
         self.dummy_state = dict()
-        self.dummy_state["cake"] = "lie"
+        self.download_rates = dict() #ADDED  dictionary
     
     def requests(self, peers, history):
         """
@@ -32,7 +32,6 @@ class Dummy(Peer):
         needed_pieces = list(filter(needed, list(range(len(self.pieces)))))
         np_set = set(needed_pieces)  # sets support fast intersection ops.
 
-
         logging.debug("%s here: still need pieces %s" % (
             self.id, needed_pieces))
 
@@ -46,24 +45,29 @@ class Dummy(Peer):
 
         requests = []   # We'll put all the things we want here
         # Symmetry breaking is good...
-        random.shuffle(needed_pieces)
+        # random.shuffle(needed_pieces)
         
         # Sort peers by id.  This is probably not a useful sort, but other 
         # sorts might be useful
-        peers.sort(key=lambda p: p.id)
-        # request all available pieces from all peers!
+        # peers.sort(key=lambda p: p.id)
+        #ADDED count how many times each piece appears
+        count_pieces = {piece: 0 for piece in np_set}
+        for peer in peers:
+            for piece in peer.available_pieces:
+                if piece in count_pieces:
+                    count_pieces[piece] += 1
+
+        #ADDED sort rarity
+        needed_pieces.sort(key=lambda piece: count_pieces[piece])
+
+        # ADDED request rarest pieces
         # (up to self.max_requests from each)
         for peer in peers:
             av_set = set(peer.available_pieces)
             isect = av_set.intersection(np_set)
             n = min(self.max_requests, len(isect))
-            # More symmetry breaking -- ask for random pieces.
-            # This would be the place to try fancier piece-requesting strategies
-            # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(sorted(isect), n):
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
+            #ADDED
+            for piece_id in sorted(isect, key=lambda piece: count_pieces[piece])[:n]:
                 start_block = self.pieces[piece_id]
                 r = Request(self.id, peer.id, piece_id, start_block)
                 requests.append(r)
@@ -84,27 +88,43 @@ class Dummy(Peer):
         round = history.current_round()
         logging.debug("%s again.  It's round %d." % (
             self.id, round))
+        if round > 0:
+            for download in history.downloads[round-1]:
+                peer_id = download.from_id
+                amount_downloaded = download.blocks
+                if peer_id not in self.download_rates:
+                     self.download_rates[peer_id] = []
+                rate = amount_downloaded / 20  # ADDED each round 20 seconds?
+                self.download_rates[peer_id].append(rate)
+            
         # One could look at other stuff in the history too here.
         # For example, history.downloads[round-1] (if round != 0, of course)
         # has a list of Download objects for each Download to this peer in
         # the previous round.
 
+        #ADDED CALC DOWNLOAD RATE
+        def calc_download(peer_id):
+            if peer_id not in self.download_rates:
+                return 0
+            return sum(self.download_rates[peer_id]) / len(self.download_rates[peer_id])
+
+        interested_peers = [r.requester_id for r in requests].sort(key=calc_download, reverse=True)
+
         if len(requests) == 0:
             logging.debug("No one wants my pieces!")
-            chosen = []
+            # chosen = []
             bws = []
         else:
-            logging.debug("Still here: uploading to a random peer")
-            # change my internal state for no reason
-            self.dummy_state["cake"] = "pie"
-
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
-
-        # create actual uploads out of the list of peer ids and bandwidths
-        uploads = [Upload(self.id, peer_id, bw)
-                   for (peer_id, bw) in zip(chosen, bws)]
+            #ADDED allocate bandwith among top requesters
+            bws = even_split(self.up_bw, len(interested_peers)) 
+        if bws is None:
+            bws = []
+        if interested_peers is None:
+            interested_peers = []
+        print("DEBUG: interested_peers =", interested_peers)
+        print("DEBUG: bws =", bws)
+        # create actual uploads out of the list of requester ids and bandwidths
+        uploads = [Upload(self.id, requester_id, bw)
+                   for (requester_id, bw) in zip(interested_peers, bws)]
             
         return uploads
