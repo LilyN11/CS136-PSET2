e67ecc686cef49add6d26ed3021f6ee4d08f85a5
LilyN11 <lilynguyen1011@gmail.com>
2023-09-28 21:17:58 -0400

    Propshare

--- millypropshare.py
+++ millypropshare.py
@@ -8,17 +8,17 @@
 
 import random
 import logging
+import numpy as np
 
 from messages import Upload, Request
 from util import even_split
 from peer import Peer
 
-class Dummy(Peer):
+class MillyPropshare(Peer):
     def post_init(self):
         print(("post_init(): %s here!" % self.id))
-        self.dummy_state = dict()
-        self.dummy_state["cake"] = "lie"
-    
+        self.optimistic = "None"
+
     def requests(self, peers, history):
         """
         peers: available info about the peers (who has what pieces)
@@ -47,12 +47,35 @@ class Dummy(Peer):
         requests = []   # We'll put all the things we want here
         # Symmetry breaking is good...
         random.shuffle(needed_pieces)
-        
+
+        count_pieces = np.zeros(self.conf.num_pieces)
+        for peer in peers:
+            intersect = np_set.intersection(peer.available_pieces)
+            if bool(intersect):
+                count_pieces[np.array(list(intersect))] +=1
+
+        needed_pieces.sort(key=lambda piece: int(count_pieces[piece]))
+
         # Sort peers by id.  This is probably not a useful sort, but other 
         # sorts might be useful
-        peers.sort(key=lambda p: p.id)
+        if len(history.uploads) == 0:
+            unblocked = []
+        else:
+            unblocked = list(np.unique([upload.to_id for upload in history.uploads[history.last_round()]]))
+
+        def rank(p):
+            if p.id in unblocked:
+                return self.conf.num_pieces + len(p.available_pieces)
+            else:
+                return len(p.available_pieces)
+
+        peers.sort(key=rank, reverse=True)
+        
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
+
+        early = (sum(self.pieces) < self.conf.blocks_per_piece)
+
         for peer in peers:
             av_set = set(peer.available_pieces)
             isect = av_set.intersection(np_set)
@@ -60,13 +83,23 @@ class Dummy(Peer):
             # More symmetry breaking -- ask for random pieces.
             # This would be the place to try fancier piece-requesting strategies
             # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(sorted(isect), n):
+            if isect is None:
+                pass
+            elif early and peer not in unblocked:
+                piece_choices = random.sample(sorted(isect), n)
+            else:
+                piece_choices = sorted(isect, key=lambda piece: needed_pieces.index(piece))[:n]
+
+            for piece_id in piece_choices:
                 # aha! The peer has this piece! Request it.
                 # which part of the piece do we need next?
                 # (must get the next-needed blocks in order)
                 start_block = self.pieces[piece_id]
                 r = Request(self.id, peer.id, piece_id, start_block)
                 requests.append(r)
+
+                needed_pieces.remove(piece_id)
+                needed_pieces.append(piece_id)
 
         return requests
 
@@ -80,31 +113,74 @@ class Dummy(Peer):
 
         In each round, this will be called after requests().
         """
+        m = 10
 
-        round = history.current_round()
-        logging.debug("%s again.  It's round %d." % (
-            self.id, round))
-        # One could look at other stuff in the history too here.
-        # For example, history.downloads[round-1] (if round != 0, of course)
-        # has a list of Download objects for each Download to this peer in
-        # the previous round.
+        if not requests:
+           return []
 
-        if len(requests) == 0:
-            logging.debug("No one wants my pieces!")
-            chosen = []
-            bws = []
-        else:
-            logging.debug("Still here: uploading to a random peer")
-            # change my internal state for no reason
-            self.dummy_state["cake"] = "pie"
+        interested = list(set(request.requester_id for request in requests))
 
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
+        generosity = {peer.id : 0 for peer in peers}
+        for download in history.downloads[-1]:
+            if download.to_id == self.id:
+                generosity[download.from_id] += download.blocks
 
-        # create actual uploads out of the list of peer ids and bandwidths
-        uploads = [Upload(self.id, peer_id, bw)
-                   for (peer_id, bw) in zip(chosen, bws)]
-            
+        chosen = sorted([peer for peer in interested if generosity[peer] > 0], key=lambda peer: generosity[peer], reverse=True)[:m-1]
+
+        total_bw_avail = np.trunc(self.up_bw * ((m-1)/m))
+        total_download = sum(generosity[p] for p in chosen)
+        bw_allocation = {}
+        for c in chosen:
+            bw_allocation[c] = np.trunc((generosity[c] / total_download) * total_bw_avail) if total_download else 0
+
+        # Optimistic unchoke
+        unchosen = [peer for peer in interested if peer not in chosen]
+        optimistic_peer = random.choice(unchosen) if unchosen else None
+        if optimistic_peer:
+            chosen.append(optimistic_peer)
+            bw_allocation[optimistic_peer] = np.trunc(self.up_bw * (1/m))
+
+        bws = [bw_allocation[peer] for peer in chosen]
+        uploads = [Upload(self.id, peer_id, bw) for (peer_id, bw) in zip(chosen, bws)]
         return uploads
+                    
+
+           
+
+        #     bw_allocation = {}
+        #     total_bw_avail = self.up_bw * ((m-1)/m)
+        #     total_download = sum([generosity[p] for p in chosen])
+        #     print("Apple")
+        #     print(total_download)
+        #     print(generosity)
+        #     for c in chosen:
+        #         print(c)
+        #         if total_download != 0:
+        #             bw_allocation[c] = np.round((generosity[c] / total_download) * total_bw_avail, 2)
+        #             print("%s Allocate" % bw_allocation )
+            
+        #     print("%s TESTING44" % bw_allocation )   
+        #     print(chosen) 
+        #     # Every round unchosen gets unchoked?
+        #     unchosen = [peer for peer in interested if peer not in chosen]
+        #     if unchosen:
+        #         optimistic_peer = random.choice(unchosen)
+        #         chosen.append(optimistic_peer)
+        #         bw_allocation[optimistic_peer] = np.round(self.up_bw * (1/m),0)
+        #     else:
+        #         if self.optimistic in interested:
+        #             chosen.append(self.optimistic)
+        #             bw_allocation[self.optimistic] = np.round(self.up_bw * (1/m),0)
+            
+        #     print("%s TESTING22" % bw_allocation )
+        #     bws = [bw_allocation[peer] for peer in chosen]
+
+        #     # Evenly "split" my upload bandwidth among the one chosen requester
+        #     # bws = even_split(self.up_bw, len(chosen))
+            
+
+        # # create actual uploads out of the list of peer ids and bandwidths
+        # uploads = [Upload(self.id, peer_id, bw)
+        #             for (peer_id, bw) in zip(chosen, bws)]
+            
+        # return uploads
